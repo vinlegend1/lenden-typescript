49cd9429b74a478eb2565d9d28b54620cec94f33
kartik <kartikbhalla12@gmail.com>
2020-08-22 23:12:02 +0530

    sign: nav: move css from component
    Signed-off-by: kartik <kartikbhalla12@gmail.com>

--- src/components/sign/signNav.tsx
+++ src/components/sign/signNav.tsx
@@ -5,16 +5,9 @@ export interface SignNavProps {}
 
 const SignNav: React.FC<SignNavProps> = () => {
 	return (
-		<div
-			style={{
-				height: '3.5rem',
-				backgroundColor: '#1a2639',
-				display: 'flex',
-				alignItems: 'center',
-				boxShadow: '0px 2px 4px rgba(26, 38, 57, 0.25)',
-			}}>
-			<LinkContainer to='/' style={{ margin: 'auto 1.5rem' }}>
-				<img src='/icons/navLogo.svg' style={{ width: '2.5rem' }} alt='' />
+		<div id='signNav'>
+			<LinkContainer to='/'>
+				<img src='/icons/navLogo.svg' alt='' />
 			</LinkContainer>
 		</div>
 	);
