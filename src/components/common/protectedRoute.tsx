b85c8b18fc280104a0784ba503d9dc5944906ff3
kartik <kartikbhalla12@gmail.com>
2020-08-25 00:27:32 +0530

    common: add protectedRoute
    Signed-off-by: kartik <kartikbhalla12@gmail.com>

---
+++ src/components/common/protectedRoute.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { getCurrentUser } from './../../services/authService';
+
+const ProtectedRoute = ({
+	component: Component,
+	render,
+	...rest
+}: {
+	component: React.ReactType;
+	render: () => React.Component;
+}) => {
+	return (
+		<Route
+			{...rest}
+			render={props => {
+				if (!getCurrentUser())
+					return (
+						<Redirect
+							to={{
+								pathname: '/login',
+								state: { from: props.location },
+							}}
+						/>
+					);
+				return Component ? <Component {...props} /> : render();
+			}}
+		/>
+	);
+};
+
+export default ProtectedRoute;
