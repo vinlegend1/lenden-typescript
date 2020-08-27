a5a0f4f8ff6d127195111b2737aeff65e0f52254
kartik <kartikbhalla12@gmail.com>
2020-08-27 20:47:53 +0530

    icons: create svg components on runtime
    Signed-off-by: kartik <kartikbhalla12@gmail.com>

---
+++ src/icons/index.tsx
@@ -0,0 +1,71 @@
+// Categories
+
+import mobile from './categories/mobile.svg';
+import book from './categories/book.svg';
+import cd from './categories/cd.svg';
+import console from './categories/console.svg';
+import accessory from './categories/accessory.svg';
+
+// Gravatars
+
+import type0 from './gravatar/user0.svg';
+import type1 from './gravatar/user1.svg';
+import type2 from './gravatar/user2.svg';
+import type3 from './gravatar/user4.svg';
+import type4 from './gravatar/user4.svg';
+
+// Generics
+
+import back from './back.svg';
+import search from './search.svg';
+import bookmark from './bookmark.svg';
+import bell from './bell.svg';
+import edit from './edit.svg';
+import cross from './cross.svg';
+import tick from './tick.svg';
+import info from './info.svg';
+import menu from './menu.svg';
+import success from './success.svg';
+import up from './up.svg';
+import down from './down.svg';
+
+// LenDen Specific
+
+import mainBanner from './main-banner.svg';
+import navLogo from './nav-logo.svg';
+
+export const categoryIcons: { [key: string]: string } = {
+	mobile,
+	book,
+	cd,
+	console,
+	accessory,
+};
+
+export const gravatarIcons: { [key: string]: string } = {
+	type0,
+	type1,
+	type2,
+	type3,
+	type4,
+};
+
+export const genericIcons: { [key: string]: string } = {
+	back,
+	search,
+	bookmark,
+	bell,
+	edit,
+	cross,
+	tick,
+	info,
+	menu,
+	success,
+	up,
+	down,
+};
+
+export const lendenSpecific: { [key: string]: string } = {
+	navLogo,
+	mainBanner,
+};
